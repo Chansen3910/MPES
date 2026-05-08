a75d6c1f70fba6201850f52060fa25a16e536108
Collin Hansen <chansen3910@gmail.com>
2026-05-08 18:28:02 -0400

    Added initial CMakeLists.txt with build options. Added a strong gitignore file for C/C++ projects.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void) {
+    std::cout << "Sup, welcome back to spyware github!" << std::endl;
+
+    std::cout << "We have no scruples against using your private repos to train ai to destroy the world and suck the fun out of everything." << std::endl;
+
+    std::cout << "See you" << std::endl;
+
+    return(0);
+}
